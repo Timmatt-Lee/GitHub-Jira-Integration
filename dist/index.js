31818b99769f58e611c4c5ff2b1490be2b4cd8a6
Timmatt <timmatt@ntu.im>
2020-08-21 21:07:28 +0800

    simple

--- dist/index.js
+++ dist/index.js
@@ -1765,7 +1765,7 @@ async function main() {
       repo: github.context.repo.repo,
       pull_number: github.context.payload.pull_request.number,
       title: `[${issue.key}] ${github.context.payload.pull_request.title}`,
-      body: `[link to ${issue.key}](${host}/browse/${issue.key})\n${github.context.payload.pull_request.body}`,
+      body: `[${issue.key}](${host}/browse/${issue.key})\n${github.context.payload.pull_request.body}`,
     };
 
     const octokit = github.getOctokit(githubToken);
