3aa8174ce99c7c7499f9552b66a91cba9f8e59be
Timmatt <timmattlee@noodoe.com>
2020-08-21 15:58:04 +0800

    feat: create issue

--- services/jira.js
+++ services/jira.js
@@ -1,8 +1,6 @@
 'user strict';
 
-const _ = require('lodash');
 const axios = require('axios');
-const joi = require('joi');
 const ErrorExit = require('./error-exit');
 
 class Jira {
@@ -12,30 +10,55 @@ class Jira {
     token,
     project,
     version,
+    component,
+    type,
   }) {
     this.host = host;
     this.token = token;
     this.email = email;
     this.project = project;
     this.version = version;
+    this.component = component;
+    this.type = type;
   }
 
   async check() {
     return this.request('/rest/api/3/myself');
   }
 
-  async getVersions(project = this.project) {
-    return this.request(`/rest/api/3/project/${project}/versions`);
+  async getVersions() {
+    return this.request(`/rest/api/3/project/${this.project}/versions`);
   }
 
-  async getVersionIdByPrefix(prefix = this.version) {
+  async getVersionIdByPrefix() {
     const { data: versions } = await this.getVersions();
-    const { id } = versions.find((v) => v.name.startsWith(prefix));
+    const { id } = versions.find((v) => v.name.startsWith(this.version));
     return id;
   }
 
-  async createIssue() {
-    return this.request('/rest/api/3/issue');
+  async createIssue(summary) {
+    const versionId = await this.getVersionIdByPrefix();
+    return this.request('/rest/api/3/issue', 'post', {
+      fields: {
+        summary,
+        project: {
+          key: this.project,
+        },
+        issuetype: {
+          name: this.type,
+        },
+        components: [
+          {
+            name: this.component,
+          },
+        ],
+        fixVersions: [
+          {
+            id: versionId,
+          },
+        ],
+      },
+    });
   }
 
   async request(api, method = 'get', data = {}) {
--- test/jira.js
+++ test/jira.js
@@ -2,7 +2,6 @@ const chai = require('chai')
   .use(require('chai-as-promised'))
   .use(require('sinon-chai'));
 const Jira = require('../services/jira');
-const ErrorExit = require('../services/error-exit');
 require('dotenv').config();
 
 const {
@@ -15,6 +14,8 @@ const email = process.env.EMAIL;
 const token = process.env.TOKEN;
 const project = process.env.PROJECT;
 const version = process.env.VERSION;
+const component = process.env.COMPONENT;
+const type = process.env.TYPE;
 
 const jira = new Jira({
   host,
@@ -22,25 +23,28 @@ const jira = new Jira({
   token,
   project,
   version,
+  component,
+  type,
 });
 
 describe('jira', () => {
   it('init', async () => {
-    const {
-      status,
-    } = await jira.check();
+    const { status } = await jira.check();
     expect(status).equal(200);
   });
 
   it('versions', async () => {
-    const {
-      status,
-    } = await jira.getVersions();
+    const { status } = await jira.getVersions();
     expect(status).equal(200);
   });
 
-  it.only('versionId', async () => {
+  it('versionId', async () => {
     const id = await jira.getVersionIdByPrefix();
     expect(id).to.not.equal(null);
+  });
+
+  it.only('create issue', async () => {
+    const { status } = await jira.createIssue('title of issue');
+    expect(status).to.not.equal(null);
   });
 });
