b3f1a4401120f6094e2a6c7ca559db41b2e40743
Timmatt <timmattlee@noodoe.com>
2020-08-21 17:31:02 +0800

    test

--- index.js
+++ index.js
@@ -3,23 +3,34 @@ const github = require('@actions/github');
 const Jira = require('./services/jira');
 const ErrorExit = require('./services/error-exit');
 
-const payload = JSON.stringify(github.context.payload, undefined, 2);
-
 // eslint-disable-next-line import/no-dynamic-require
 const githubEvent = require(process.env.GITHUB_EVENT_PATH);
 
 async function main() {
   const host = core.getInput('host');
-  const token = core.getInput('token');
   const email = core.getInput('email');
+  const token = core.getInput('token');
+  const project = core.getInput('project');
+  const version = core.getInput('version');
+  const component = core.getInput('component');
+  const type = core.getInput('type');
+  const board = core.getInput('board');
+  const isCreateIssue = core.getInput('isCreateIssue');
 
   const jira = new Jira({
     host,
     email,
     token,
+    project,
+    version,
+    component,
+    type,
+    board,
   });
 
   const check = await jira.check();
+
+  console.log(github.context.payload);
 
   if (!check) {
     ErrorExit.trigger(ErrorExit.INIT);
