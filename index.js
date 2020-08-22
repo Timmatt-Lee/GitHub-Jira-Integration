b35e6d5f377770ea2ce2b393c3a444a99d239bfa
Timmatt-Lee <43662873+Timmatt-Lee@users.noreply.github.com>
2020-08-22 10:14:25 +0800

    [NE-2605]merge (#5)
    * merge
    
    * t
    
    * dev
    
    * t
    
    * t
    
    * y
    
    * y
    
    * key
    
    * 1
    
    * rm
    
    * c
    
    * final

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const github = require('@actions/github');
 const Jira = require('./services/jira');
 
 async function main() {
-  const host = core.getInput('host');
-  const email = core.getInput('email');
-  const token = core.getInput('token');
-  const project = core.getInput('project');
+  const host = core.getInput('host', { required: true });
+  const email = core.getInput('email', { required: true });
+  const token = core.getInput('token', { required: true });
+  const project = core.getInput('project', { required: true });
+  const transition = core.getInput('transition', { required: true });
+  const githubToken = core.getInput('githubToken');
   const version = core.getInput('version');
   const component = core.getInput('component');
   const type = core.getInput('type');
   const board = core.getInput('board');
-  const transition = core.getInput('transition');
-  const githubToken = core.getInput('githubToken');
+  const isOnlyTransition = core.getInput('isOnlyTransition').toLowerCase() === 'true';
+  const isNotCreateIssue = core.getInput('isNotCreateIssue').toLowerCase() === 'true';
 
   const jira = new Jira({
     host,
@@ -25,67 +27,68 @@ async function main() {
     board,
   });
 
-  try {
-    const pr = github.context.payload.pull_request;
-    if (!pr) {
-      core.setFailed('Only support pull request trigger');
-    }
-
-    let key = '';
-
-    // if title has a [AB-1234] like Jira issue tag
-    const keyWithBracket = pr.title.match(`\\[${project}-\\d+\\]`);
-    if (keyWithBracket) {
-      key = keyWithBracket[0].substring(1, keyWithBracket[0].length - 1);
-    } else {
-      const issue = await jira.postIssue(pr.title);
-      key = issue.key;
-
-      // move card to active sprint
-      const { values: [{ id: activeSprintId }] } = await jira.getSprints('active');
-      await jira.postMoveIssuesToSprint([key], activeSprintId);
-    }
-
-    if (!key) {
-      core.setFailed('Issue key parse error');
-    }
-
-    await jira.postTransitIssue(key, transition);
-
-    await jira.postComment(key, {
-      type: 'doc',
-      version: 1,
-      content: [
-        {
-          type: 'blockCard',
-          attrs: {
-            url: pr.html_url,
-          },
-        },
-      ],
-    });
-
-    // update pull request title and desc
-    const newPR = {
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      pull_number: pr.number,
-      title: `[${key}] ${pr.title}`,
-      body: `[${key}](${host}/browse/${key})\n${pr.body}`,
-    };
-
-    // if title already has jira issue, no need to update it
-    if (keyWithBracket) {
-      delete newPR.title;
-    }
-
-    const octokit = github.getOctokit(githubToken);
-    const response = await octokit.pulls.update(newPR);
-
-    if (response.status !== 200) { core.setFailed(JSON.stringify(response)); }
-  } catch (e) {
-    core.setFailed(e);
+  const pr = github.context.payload.pull_request;
+  if (!pr) {
+    core.setFailed('Only support pull request trigger');
   }
+
+  let key = '';
+
+  // if title has a [AB-1234] like Jira issue tag
+  const keyWithBracket = pr.title.match(`\\[${project}-\\d+\\]`);
+  if (keyWithBracket) {
+    key = keyWithBracket[0].substring(1, keyWithBracket[0].length - 1);
+  } else {
+    if (isNotCreateIssue) { process.exit(0); }
+    if (isOnlyTransition) { throw new Error('Need a valid Jira issue key in your title'); }
+
+    const issue = await jira.postIssue(pr.title);
+    key = issue.key;
+
+    // move card to active sprint
+    const { values: [{ id: activeSprintId }] } = await jira.getSprints('active');
+    await jira.postMoveIssuesToSprint([key], activeSprintId);
+  }
+
+  if (!key) {
+    core.setFailed('Issue key parse error');
+  }
+
+  await jira.postTransitIssue(key, transition);
+
+  if (isOnlyTransition) { process.exit(0); }
+
+  await jira.postComment(key, {
+    type: 'doc',
+    version: 1,
+    content: [
+      {
+        type: 'blockCard',
+        attrs: {
+          url: pr.html_url,
+        },
+      },
+    ],
+  });
+
+  // update pull request title and desc
+  const newPR = {
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: pr.number,
+    title: `[${key}] ${pr.title}`,
+    body: `[${key}](${host}/browse/${key})\n${pr.body}`,
+  };
+
+  // if title already has jira issue, no need to update it
+  if (keyWithBracket) {
+    delete newPR.title;
+  }
+
+  const octokit = github.getOctokit(githubToken);
+  const response = await octokit.pulls.update(newPR);
+
+  if (response.status !== 200) { core.setFailed(JSON.stringify(response)); }
 }
 
-main();
+main().catch(core.setFailed);
