1fee522ac326cc92bca9dea0702524bad1d76513
Timmatt <timmatt@ntu.im>
2020-08-22 10:06:12 +0800

    y

--- dist/index.js
+++ dist/index.js
@@ -1749,12 +1749,11 @@ async function main() {
       core.setFailed('Only support pull request trigger');
     }
 
-    core.info(pr.title);
-
     let key = '';
 
     // if title has a [AB-1234] like Jira issue tag
     const keyWithBracket = pr.title.match(`\\[${project}-\\d+\\]`);
+    core.info(keyWithBracket);
     if (keyWithBracket) {
       key = keyWithBracket[0].substring(1, keyWithBracket[0].length - 1);
     } else {
