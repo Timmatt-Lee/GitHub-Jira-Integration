099abb1672839b96b2fb49f02dcba5aabac0e9b4
Timmatt <timmatt@ntu.im>
2020-08-23 13:54:26 +0800

    error

--- dist/index.js
+++ dist/index.js
@@ -479,7 +479,24 @@ class Jira {
         username: this.email,
         password: this.token,
       },
-    }).catch((e) => Promise.reject(e));
+    }).catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        return Promise.reject(new Error(JSON.stringify({
+          data: error.response.data,
+          status: error.response.status,
+          headers: error.response.headers,
+        })));
+      } if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        return Promise.reject(error.request);
+      }
+      // Something happened in setting up the request that triggered an Error
+      return Promise.reject(error.message);
+    });
 
     return result;
   }
